fde0a7247888964b88c816d868f0347a45c65c13
Edu <ed.afram@gmail.com>
2025-06-10 20:18:28 -0300

    formating

--- samples/Easy.Mediator.Sample.Publish/NotificationHandlers/SmsNotificationHandler.cs
+++ samples/Easy.Mediator.Sample.Publish/NotificationHandlers/SmsNotificationHandler.cs
@@ -5,6 +5,7 @@ public class SmsNotificationHandler : INotificationHandler<NewUserNotification>
     public Task Handle(NewUserNotification notification, CancellationToken cancellationToken = default)
     {
         Console.WriteLine($"[SMS] To: {notification.UserName} - {notification.Message}");
+
         return Task.CompletedTask;
     }
 }
