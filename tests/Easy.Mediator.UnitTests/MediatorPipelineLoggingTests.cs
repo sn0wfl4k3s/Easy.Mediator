a0f6120d81a585f9f6b672856fa3c6f7caaf1394
Edu <ed.afram@gmail.com>
2025-07-06 15:46:11 -0300

    inclusion of more tests

--- tests/Easy.Mediator.UnitTests/MediatorPipelineLoggingTests.cs
+++ tests/Easy.Mediator.UnitTests/MediatorPipelineLoggingTests.cs
@@ -31,4 +31,47 @@ public class MediatorPipelineLoggingTests
         Assert.Equal("test => Pong!", response.Message);
         Assert.True(LoggingBehavior<PingCommand, PongResponse>.Executed);
     }
+
+    [Fact]
+    public void Executed_Should_Be_False_Before_Any_Execution()
+    {
+        Assert.False(LoggingBehavior<PingCommand, PongResponse>.Executed);
+    }
+
+    [Fact]
+    public async Task Send_Should_Set_Executed_True_After_Handler()
+    {
+        LoggingBehavior<PingCommand, PongResponse>.Executed = false;
+        var command = new PingCommand("executed");
+        await _mediator.Send(command);
+
+        Assert.True(LoggingBehavior<PingCommand, PongResponse>.Executed);
+    }
+
+    [Fact]
+    public async Task Send_Should_Reset_Executed_Between_Executions()
+    {
+        LoggingBehavior<PingCommand, PongResponse>.Executed = false;
+
+        var command = new PingCommand("first");
+        await _mediator.Send(command);
+        Assert.True(LoggingBehavior<PingCommand, PongResponse>.Executed);
+
+        LoggingBehavior<PingCommand, PongResponse>.Executed = false;
+
+        var command2 = new PingCommand("second");
+        await _mediator.Send(command2);
+        Assert.True(LoggingBehavior<PingCommand, PongResponse>.Executed);
+    }
+
+    [Fact]
+    public async Task Send_Should_Not_Throw_And_Executed_Should_Be_True()
+    {
+        LoggingBehavior<PingCommand, PongResponse>.Executed = false;
+        var command = new PingCommand("safe");
+        var response = await _mediator.Send(command);
+
+        Assert.NotNull(response);
+        Assert.True(LoggingBehavior<PingCommand, PongResponse>.Executed);
+    }
 }
--- tests/Easy.Mediator.UnitTests/MediatorPipelineValidationTests.cs
+++ tests/Easy.Mediator.UnitTests/MediatorPipelineValidationTests.cs
@@ -36,4 +36,34 @@ public class MediatorPipelineValidationTests
         Assert.Equal("Name is required", ex.Message);
         Assert.False(UserCreateHandler.WasCalled);
     }
+
+    [Fact]
+    public async Task Send_Should_Invoke_Handler_When_Name_Is_Valid()
+    {
+        // Arrange
+        UserCreateHandler.WasCalled = false;
+
+        var validCommand = new UserCreateCommand("Valid Name");
+
+        // Act
+        await _mediator.Send(validCommand);
+
+        // Assert
+        Assert.True(UserCreateHandler.WasCalled);
+    }
+
+    [Fact]
+    public async Task Send_Should_Throw_When_Command_Is_Null()
+    {
+        // Arrange
+        UserCreateHandler.WasCalled = false;
+
+        UserCreateCommand nullCommand = null;
+
+        // Act & Assert
+        var ex = await Assert.ThrowsAsync<NullReferenceException>(() => _mediator.Send(nullCommand));
+
+        Assert.Equal(typeof(NullReferenceException), ex.GetType());
+        Assert.False(UserCreateHandler.WasCalled);
+    }
 }
